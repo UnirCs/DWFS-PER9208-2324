40a74182553e2b8abc10e7ef646af81d839ec948
cristiandelacruz90 <cristian.delacruz@ipsp-profremar.com>
2024-04-14 00:00:06 -0500

    Correcion cinema tema 2

--- Tema_2/Cristian_De_La_Cruz_Mendoza/cinema/cinema.js
+++ Tema_2/Cristian_De_La_Cruz_Mendoza/cinema/cinema.js
@@ -27,32 +27,37 @@ function setup() {
 function suggest(num_seats) {
     // Inicializar la matriz de butacas
     let butacas = setup();
+    let selected_seats = new Set();
+    let found = true
+
+    // Si el número de asientos solicitados excede el tamaño máximo de la fila, devolver un set vacío
+    if (num_seats > N) {
+        return selected_seats;
+    }
 
     // Encontrar asientos disponibles
-    let selected_seats = new Set();
     for (let i = N - 1; i >= 0; i--) { // Comenzar desde la última fila (la más lejana a la pantalla)
-        let available_seats = [];
         let consecutive_count = 0;
+        let available_seats = [];
         for (let j = 0; j < N; j++) {
             if (!butacas[i][j].estado) {
                 consecutive_count++;
                 available_seats.push(butacas[i][j].id);
-                if (consecutive_count === num_seats) {
-                    selected_seats = new Set([...selected_seats, ...available_seats]);
-                    break;
+                if (consecutive_count === num_seats && found===true) {
+                    selected_seats = new Set(available_seats);
+                    found = false
+                    //return selected_seats;
                 }
             } else {
                 consecutive_count = 0;
                 available_seats = [];
             }
         }
-        if (selected_seats.size === num_seats) {
-            break;
-        }
     }
 
     return selected_seats;
 }
+
 
 console.log(setup())
 
