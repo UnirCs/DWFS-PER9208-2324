563659adb050870c501b8825bf76f49271f4769f
MiloVirus <cami_alejo6@hotmail.com>
2024-04-15 19:08:19 -0500

    Tema 2 - Cinema

---
+++ Tema_2/Camilo_Valencia/cinema/cinema.js
@@ -0,0 +1,55 @@
+// Definir el tamaño de la matriz de butacas
+const N = 4; // Número de filas y columnas
+
+// Función para inicializar la matriz de butacas
+function setup() {
+  let idContador = 1; // Iniciar el contador de IDs en 1 (los humanos no empezamos a contar desde 0)
+  let butacas = [];
+
+  for (let i = 0; i < N; i++) {
+    // Nueva fila
+    let fila = [];
+    for (let j = 0; j < N; j++) {
+      // Nuevo asiento
+      fila.push({
+        id: idContador++,
+        estado: false, // Estado inicial libre
+      });
+    }
+    butacas.push(fila);
+  }
+  return butacas;
+}
+
+const suggest = (numSeats) => {
+  const numberofSeats = new Set();
+  if (numSeats <= N) {
+    let consecutiveSeats = 0;
+    const rows = butacas.slice().reverse();
+    for (let i = 0; i < rows.length && numberofSeats.size < numSeats; i++) {
+      const row = rows[i];
+      for (let j = 0; j < row.length && numberofSeats.size < numSeats; j++) {
+        const seat = row[j];
+        if (!seat.estado) {
+          consecutiveSeats++;
+          numberofSeats.add(seat.id);
+          if (consecutiveSeats === numSeats) {
+            return numberofSeats;
+          }
+        } else {
+          consecutiveSeats = 0;
+          numberofSeats.clear();
+        }
+      }
+    }
+  }
+  return numberofSeats;
+};
+
+// Inicializar la matriz
+let butacas = setup();
+
+// Imprimir la matriz
+console.log(butacas);
+
+console.log(suggest(3));
