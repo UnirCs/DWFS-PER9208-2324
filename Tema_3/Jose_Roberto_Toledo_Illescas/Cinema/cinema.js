1ce33bce1d4e184f5f123a672361832beb3e423b
JoseToledo046 <joseroberto.toledo046@comunidadunir.net>
2024-04-18 05:11:07 -0500

    'Tareas'

---
+++ Tema_3/Jose_Roberto_Toledo_Illescas/Cinema/cinema.js
@@ -0,0 +1,66 @@
+function setup() {
+
+    let idContador = 1; 
+    let butacas = [];
+
+    for (let i = 0; i < N; i++) {
+
+        let fila = [];
+        for (let j = 0; j < N; j++) {
+
+            fila.push({
+                id: idContador++,
+                estado: false 
+            });
+        }
+        butacas.push(fila);
+    }
+    return butacas;
+}
+
+function printButacas(data){
+    console.log(data);
+}
+
+function updateButacas(){
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+            butacas[i][j].estado = Math.random() < 0.5;
+        }
+    }
+}
+
+function suggest(solicitud){
+
+    let contador = 0;
+    let temp = [];
+
+    if (solicitud > N) {
+        return temp;
+    } 
+
+    for(let i = N-1; i > 0; i--){
+        for(let j = 0; j < N; j++){
+            if(butacas[i][j].estado == false){
+                contador++;
+                temp.push(butacas[i][j].id);
+                if(contador==solicitud){
+                    return temp;
+                } 
+            } else{
+                contador = 0;
+                temp = [];
+            }
+        }
+    }
+}
+
+
+function resSugerir(valorButaca){
+    updateButacas();
+    const reserva = suggest(valorButaca);
+    printButacas(reserva);
+}
+
+const N = 10; 
+let butacas = setup();
---
+++ Tema_4/Jose_Roberto_Toledo_Illescas/cinema/cinema.js
@@ -0,0 +1,97 @@
+function setup() {
+
+    let idContador; 
+    let butacas = [];
+
+    for (let i = 0; i < fil; i++) {
+
+        let fila = [];
+        for (let j = 0; j < col; j++) {
+
+            fila.push({
+                id: i.toString() + '-' + j.toString(),
+                estado: Math.random() < 0.5
+            });
+
+            if(fila[j].estado){
+                const asientoOcupado = document.getElementById(i.toString() + '-' + j.toString());
+                asientoOcupado.classList.add('ocupado');
+            }
+        }
+        butacas.push(fila);
+    }
+    return butacas;
+}
+
+function printButacas(data){
+    console.log(data);
+}
+
+function suggest(solicitud){
+
+    let contador = 0;
+    let temp = [];
+
+    if (solicitud > col) {
+        return temp;
+    } 
+
+    for(let i = fil-1; i > 0; i--){
+        for(let j = 0; j < col; j++){
+            if(butacas[i][j].estado == false){
+                contador++;
+                temp.push(butacas[i][j].id);
+                if(contador==solicitud){
+                    colorReserva(temp);
+                    return temp;
+                } else{
+                    colorNormal(temp);
+                }
+            } else{
+                contador = 0;
+                temp = [];
+            }
+        }
+    }
+}
+
+function colorReserva(reserva){
+
+    let posReserva = reserva.length;
+
+    if(posReserva > 0){
+        for(let i = 0; i < posReserva; i++){
+            const asientoReservado = document.getElementById(reserva[i]);
+            asientoReservado.classList.add('reserva');
+        }
+    }
+
+}
+
+function colorNormal(reserva){
+
+    let posReserva = reserva.length;
+
+    if(posReserva > 0){
+        for(let i = 0; i < posReserva; i++){
+            const asientoReservado = document.getElementById(reserva[i]);
+            asientoReservado.classList.remove('reserva');
+        }
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('valorSugerido').addEventListener('input', () =>{
+
+        const reserva = suggest(document.getElementById('valorSugerido').value);
+
+    });
+
+});
+
+const  fil = 5;
+const col = 12;
+const valSug = document.getElementById('valorSugerido');
+let butacas = setup();
