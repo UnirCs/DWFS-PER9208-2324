5aa27d6650715db0ee25af464be0bdb9a17e5637
Heltus <1967937-heltus@users.noreply.gitlab.com>
2024-04-22 20:50:43 -0500

    Tema 5

--- Tema_5/Hector_Suarez/cinema/src/components/Footer.js
+++ Tema_5/Hector_Suarez/cinema/src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+
 export const Footer = () => {
     return (
         <footer className="footer">
-            <p>© 2023 UNIR Delivery.</p>
+            <div className="content has-text-centered">
+                <p>© 2023 UNIR Delivery.</p>
+            </div>
         </footer>
     );
 }
