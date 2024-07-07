59ded26d73bb9dc946cc7246da47f37fd91e3682
Gabriel Martinez <gabriel.martinez945@comunidadunir.net>
2024-07-07 16:58:15 +0200

    Tema 6: calculadora - fix Tema 8: calculadora - swagger

--- Tema_8/Gabriel_Martinez_Pieper/calculator-api-mongodb/src/main/java/com/unir/calculatorapimongodb/controller/CalculatorController.java
+++ Tema_8/Gabriel_Martinez_Pieper/calculator-api-mongodb/src/main/java/com/unir/calculatorapimongodb/controller/CalculatorController.java
@@ -3,6 +3,11 @@ package com.unir.calculatorapimongodb.controller;
 import com.unir.calculatorapimongodb.model.*;
 import com.unir.calculatorapimongodb.model.request.*;
 import com.unir.calculatorapimongodb.service.CalculatorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,41 +22,186 @@ public class CalculatorController {
     private final CalculatorService service;
 
     @PostMapping("/additions")
+    @Operation(
+            operationId = "Realizar una suma",
+            description = "Operacion de escritura",
+            summary = "Se realiza una suma de n numeros y se crea un registro de la operación.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Numeros a sumar.",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdditionSubtractionRequest.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdditionSubtractionResult.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
     public ResponseEntity<AdditionSubtractionResult> add(@RequestBody AdditionSubtractionRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.add(request));
+        AdditionSubtractionResult addition = service.add(request);
+        if (addition != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(addition);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/subtractions")
+    @Operation(
+            operationId = "Realizar una resta",
+            description = "Operacion de escritura",
+            summary = "Se realiza una resta de n numeros y se crea un registro de la operación.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Numeros a sumar.",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdditionSubtractionRequest.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdditionSubtractionResult.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
     public ResponseEntity<AdditionSubtractionResult> subtract(@RequestBody AdditionSubtractionRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.subtract(request));
+        AdditionSubtractionResult subtraction = service.subtract(request);
+        if (subtraction != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(subtraction);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/multiplications")
+    @Operation(
+            operationId = "Realizar una multiplicación",
+            description = "Operacion de escritura",
+            summary = "Se realiza una multiplicación de dos numeros y se crea un registro de la operación.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Numeros a multiplicar.",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = MultiplicationRequest.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MultiplicationResult.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
     public ResponseEntity<MultiplicationResult> multiply(@RequestBody MultiplicationRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.multiply(request));
+        MultiplicationResult multiplication = service.multiply(request);
+        if (multiplication != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(multiplication);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/divisions")
+    @Operation(
+            operationId = "Realizar una división",
+            description = "Operacion de escritura",
+            summary = "Se realiza una división y se crea un registro de la operación.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Dividendo y divisor.",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = DivisionRequest.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = DivisionResult.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
     public ResponseEntity<DivisionResult> divide(@RequestBody DivisionRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.divide(request));
+        DivisionResult division = service.divide(request);
+        if (division != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(division);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/roots")
+    @Operation(
+            operationId = "Realizar una raiz",
+            description = "Operacion de escritura",
+            summary = "Se realiza una raiz y se crea un registro de la operación.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "radicando e indice",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = RootRequest.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = RootResult.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
     public ResponseEntity<RootResult> divide(@RequestBody RootRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.root(request));
+        RootResult root = service.root(request);
+        if (root != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(root);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/powers")
+    @Operation(
+            operationId = "Realizar una potencia",
+            description = "Operacion de escritura",
+            summary = "Se realiza una potencia y se crea un registro de la operación.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "base y exponente",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = PowerRequest.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PowerResult.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
     public ResponseEntity<PowerResult> power(@RequestBody PowerRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.power(request));
+        PowerResult power = service.power(request);
+        if (power != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(power);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/operations")
-    public ResponseEntity<List<Result>> getOperations(@RequestParam(required = false) String type){
+    @Operation(
+            operationId = "Obtener operaciones",
+            description = "Operacion de lectura",
+            summary = "Se devuelve una lista de todos las operaciones almacenados en la base de datos.")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Result.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
+    public ResponseEntity<List<Result>> getOperations(
+            @Parameter(name = "type", description = "Tipo de operación", example = "addition", required = false)
+            @RequestParam(required = false) String type
+    ){
         return ResponseEntity.ok(service.getOperations(type));
     }
 
     @GetMapping("/operations/{id}")
+    @Operation(
+            operationId = "Obtener una operación",
+            description = "Operacion de lectura",
+            summary = "Se devuelve una operación a partir de su identificador.")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Result.class)))
+    @ApiResponse(
+            responseCode = "404",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "No se ha encontrado el producto con el identificador indicado.")
     public ResponseEntity<Result> getOperation(@PathVariable String id){
         Result result = service.getOperation(id);
         if (result == null) {
